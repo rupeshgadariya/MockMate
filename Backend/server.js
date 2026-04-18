524dd5e1ed4b073f2d94f480f8fc1b08571746b4
Rupesh Gadariya <gadariyarupesh@gmail.com>
2026-04-18 15:38:56 +0530

    Backend Done

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/auth", require("./routes/auth.routes"));
