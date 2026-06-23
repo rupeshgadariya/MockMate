f1b0a6d9db879ad575b3b58f22fdfb056771f215
Rupesh Kumar <gadariyarupesh@gmail.com>
2026-06-23 14:51:01 +0530

    Update API_URL to include '/api' endpoint

--- Fontend/js/config.js
+++ Fontend/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-  API_URL: "https://mockmate-j352.onrender.com",
+  API_URL: "https://mockmate-j352.onrender.com/api",
 
   TOKEN_KEY: "mock_interview_token",
   USER_KEY: "mock_interview_user",
