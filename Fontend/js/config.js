e55a0c6e23a01982fcfa3c2979d8bf7a26a6ccca
Rupesh Kumar <gadariyarupesh@gmail.com>
2026-06-23 14:36:49 +0530

    Update config.js

--- Fontend/js/config.js
+++ Fontend/js/config.js
@@ -1,7 +1,6 @@
 const CONFIG = {
 
-  API_URL: "https://mockmate-j352.onrender.com/",
-
+  API_URL: "https://mockmate-j352.onrender.com",
 
   TOKEN_KEY: "mock_interview_token",
   USER_KEY: "mock_interview_user",
