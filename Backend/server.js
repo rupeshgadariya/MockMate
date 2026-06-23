0cb65d46a0baa098c9bd67c0520100c1b5a8e372
Rupesh Kumar <gadariyarupesh@gmail.com>
2026-06-23 15:01:15 +0530

    Add allowed origin for Vercel deployment

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    https://mock-mate-five-rho.vercel.app/,
     process.env.FRONTEND_URL
 ].filter(Boolean);
 
